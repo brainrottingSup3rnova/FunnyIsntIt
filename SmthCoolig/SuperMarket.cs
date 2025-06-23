3081ddcef585d0ac916c3855c5020b13bfc6cdd2
brainrottingSup3rnova <anna.tassinari.stud@ispascalcomandini.it>
2025-06-23 15:12:32 +0200

    added .gitignore

---
+++ SmthCoolig/SuperMarket.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmthCoolig
+{
+    public class SuperMarket
+    {
+        public List<Products> Products;
+
+        public SuperMarket(Products[] ListOfProducts)
+        {
+            Products = new List<Products>();
+        }
+    }
+}
